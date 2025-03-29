a66c6e9ee60530a04b4e4c17503fa57360507f03
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-03-29 12:34:16 -0300

    feat: create test

--- src/test/java/com/travel/itinerary/roadmap/RoadmapApplicationTests.java
+++ src/test/java/com/travel/itinerary/roadmap/RoadmapApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class RoadmapApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		RoadmapApplication.main(new String[] {});
 	}
 
 }
